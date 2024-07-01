3e097989f12b1c0265ee5f45a8b914bcd1f57a1c
and m <andriy.myakush@gmail.com>
2024-07-01 19:46:55 +0100

    moving files

--- ex_8/backoffice/registaLivro.php
+++ ex_8/backoffice/registaLivro.php
@@ -1,36 +1,61 @@
 <?php
 
-include("../objetos/livros/Livros.php");
+    include("../objetos/livros/Livros.php");
 
-include('../config.php');
+    include('../config.php');
 
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
 
-echo "<pre>";
-print_r($_FILES);
-echo "</pre>";
+    echo "<pre>";
+    print_r($_FILES);
+    echo "</pre>";
 
-$livro = new Livro(
-    null,
-    $_POST['editora'],
-    1,
-    $_POST['titulo'],
-    $_POST['sinopse'],
-    $_POST['isbn'],
-    $_POST['preco'],
-    $_POST['stock'],
-    $_POST['n_paginas'],
-    null,
-    null,
-    null,
-    $_FILES['capa_livro']['name']
-);
+    
 
+    $nome_do_ficheiro = $_FILES['capa_livro']['name'];
 
-echo "<pre>";
-print_r($livro);
-echo "</pre>";
+    $nome_do_ficheiro_explodido = explode('.', $nome_do_ficheiro);
 
-$livro->save($ligacao);
+    $extensao = $nome_do_ficheiro_explodido[count($nome_do_ficheiro_explodido) - 1];
+
+    $novo_nome = uniqid();
+
+    $novo_nome_completo = "$novo_nome.$extensao";
+
+    $destino = "../imagens/$novo_nome_completo";
+
+    $origem = $_FILES['capa_livro']['tmp_name'];
+
+    echo "origem - " . $origem;
+    echo "<br>";
+    echo "destino - " . $destino;
+
+    $movido = move_uploaded_file($origem, $destino);
+
+    echo "<br>movido - " . $movido . "<br>";
+
+    $livro = new Livro(
+        null,
+        $_POST['editora'],
+        1,
+        $_POST['titulo'],
+        $_POST['sinopse'],
+        $_POST['isbn'],
+        $_POST['preco'],
+        $_POST['stock'],
+        $_POST['n_paginas'],
+        null,
+        null,
+        null,
+        $novo_nome_completo 
+    );
+
+    echo "<pre>";
+    print_r($livro);
+    echo "</pre>";
+
+    //$livro->save($ligacao);
+
+?>
